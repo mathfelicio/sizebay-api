0aa1fb2650624dcf7cacf4cf02d6b218f910bb86
Matheus Felicio <matheus.felicio@grupoimagetech.com.br>
2026-03-19 18:28:50 -0400

    fix: use typeorm method in migration instead of raw sql

--- src/database/migrations/1773937476941-CreateShortUrlsTable.ts
+++ src/database/migrations/1773937476941-CreateShortUrlsTable.ts
@@ -1,13 +1,48 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateShortUrlsTable1773937476941 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "short_urls",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "code",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "original_url",
+            type: "varchar",
+          },
+          {
+            name: "short_url",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            isNullable: true,
+          },
+        ],
+      }),
+      true,
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "short_urls" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "code" character varying NOT NULL, "originalUrl" character varying NOT NULL, "shortUrl" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL, CONSTRAINT "UQ_36d7fc390c3e722f91a46838833" UNIQUE ("code"), CONSTRAINT "PK_0bee0ef97594699927c1b7c81a3" PRIMARY KEY ("id"))`);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "short_urls"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("short_urls");
+  }
 }
