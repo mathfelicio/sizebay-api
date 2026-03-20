03ca6b4fa4ed9026c36886d4d1f0aa4012db2d99
Matheus Felicio <matheus.felicio@grupoimagetech.com.br>
2026-03-19 21:09:22 -0400

    feat: add swagger docs and global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module.js";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle("API de Encurtador de URLs da Sizebay")
+    .setDescription("Documentação da API para o serviço de Encurtador de URLs.")
+    .setVersion("1.0")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, documentFactory);
+
   const port = Number(process.env.PORT || 3050);
 
   await app.listen(port, "0.0.0.0");
